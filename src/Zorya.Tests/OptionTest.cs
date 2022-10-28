ea2c7022f42df222d11e914157ffa1fc2f894ffb
Dominik Andrzejczak <dersei0248@gmail.com>
2022-10-28 17:50:36 +0200

    Implementation and tests

---
+++ src/Zorya.Tests/OptionTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace Zorya.Tests;
+
+public class OptionTest
+{
+    [Test]
+    public void CreateSome()
+    {
+        var option = Option<string>.Some("test");
+        Assert.IsTrue(option.IsSome(out _));
+    }
+    
+    [Test]
+    public void CreateSomeWithNull()
+    {
+        var option = Option<string>.Some(null!);
+        Assert.IsFalse(option.IsSome(out _));
+    }
+    
+    [Test]
+    public void CreateNone()
+    {
+        var option = Option<string>.None;
+        Assert.IsFalse(option.IsSome(out _));
+    }
+}
--- src/Zorya/Option.cs
+++ src/Zorya/Option.cs
@@ -1,10 +1,62 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Zorya;
 
-public readonly struct Option<T> where T : class?
+public readonly struct Option<T> where T : notnull
 {
-    private readonly T? _value;
+    private readonly T _value;
+    private readonly bool _isSome;
 
-    public Option(T value) => _value = value;
+    private Option(T value)
+    {
+        _value = value;
+        _isSome = true;
+    }
 
-    public bool IsSet => _value != null;
+    public bool IsSome([MaybeNullWhen(false)] out T value)
+    {
+        value = _value;
+        return _isSome;
+    }
+
+    public static Option<T> None => default;
+
+    public static Option<T> Some(T value)
+    {
+        if (value is null) return None;
+        return new(value);
+    }
+}
+
+public static class Option
+{
+    public static TValue Match<T, TValue>(this Option<T> option, Func<T, TValue> onSome, Func<TValue> onNone)
+        where T : notnull
+    {
+        return option.IsSome(out var value) ? onSome(value) : onNone();
+    }
+
+    public static Option<TValue> Bind<T, TValue>(this Option<T> option, Func<T, Option<TValue>> binder)
+        where T : notnull where TValue : notnull
+    {
+        return option.Match(onSome: binder, onNone: () => Option<TValue>.None);
+    }
+
+    public static Option<TValue> Map<T, TValue>(this Option<T> option, Func<T, TValue> mapper)
+        where T : notnull where TValue : notnull
+    {
+        return option.Bind(value => Option<TValue>.Some(mapper(value)));
+    }
+
+    public static Option<T> Filter<T>(this Option<T> option, Predicate<T> predicate)
+        where T : notnull
+    {
+        return option.Bind(value => predicate(value) ? option : Option<T>.None);
+    }
+
+    public static T DefaultValue<T>(this Option<T> option, T defaultValue)
+        where T : notnull
+    {
+        return option.Match(onSome: value => value, onNone: () => defaultValue);
+    }
 }
